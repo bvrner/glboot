9290c616d99d2a87d310cad1adb610b677d11f9b
Jonatas <jbmagalhaes28@gmail.com>
2020-05-17 22:51:31 -0300

    Small obj loading optmization

--- benches/models_bench.rs
+++ benches/models_bench.rs
@@ -3,12 +3,17 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use glboot::core::window::Window;
 use glboot::ogl::model::loaders;
 
+use std::time::Duration;
+
 pub fn obj_loader(c: &mut Criterion) {
     let mut w = Window::new("bench", (1, 1));
     w.load_gl();
 
     let path = format!("{}/assets/models/teapot.obj", env!("CARGO_MANIFEST_DIR"));
-    c.bench_function("model", |b| b.iter(|| loaders::load_obj(&path).unwrap()));
+    let mut group = c.benchmark_group("Model");
+    group.measurement_time(Duration::from_secs(10));
+    group.bench_function("obj", |b| b.iter(|| loaders::load_obj(&path).unwrap()));
+    group.finish();
 }
 
 criterion_group!(models, obj_loader);
--- src/ogl/model/loaders.rs
+++ src/ogl/model/loaders.rs
@@ -15,63 +15,65 @@ where
     P: AsRef<Path> + Debug,
 {
     let (models, _materials) = tobj::load_obj(path, true).unwrap();
-    let mut meshs = vec![];
+    let meshs: Vec<Mesh> = models
+        .into_iter()
+        .map(|model| {
+            let mesh = model.mesh;
+            let num_vertices = mesh.positions.len() / 3;
 
-    for model in models.into_iter() {
-        let mesh = model.mesh;
-        let num_vertices = mesh.positions.len() / 3;
+            // data to fill
+            let mut vertices: Vec<Vertex> = Vec::with_capacity(num_vertices);
+            let indices: Vec<u32> = mesh.indices;
 
-        // data to fill
-        let mut vertices: Vec<Vertex> = Vec::with_capacity(num_vertices);
-        let indices: Vec<u32> = mesh.indices;
+            let (p, n, t) = (mesh.positions, mesh.normals, mesh.texcoords);
 
-        let (p, n, t) = (mesh.positions, mesh.normals, mesh.texcoords);
-        for i in 0..num_vertices {
-            vertices.push(Vertex {
-                vertice: vec3(
-                    p.get(i * 3).copied().unwrap_or_default(),
-                    p.get(i * 3 + 1).copied().unwrap_or_default(),
-                    p.get(i * 3 + 2).copied().unwrap_or_default(),
-                ),
-                normal: vec3(
-                    n.get(i * 3).copied().unwrap_or_default(),
-                    n.get(i * 3 + 1).copied().unwrap_or_default(),
-                    n.get(i * 3 + 2).copied().unwrap_or_default(),
-                ),
-                tex_coords: vec2(
-                    t.get(i * 2).copied().unwrap_or_default(),
-                    t.get(i * 2 + 1).copied().unwrap_or_default(),
-                ),
-            });
-        }
+            for i in 0..num_vertices {
+                vertices.push(Vertex {
+                    vertice: vec3(
+                        p.get(i * 3).copied().unwrap_or_default(),
+                        p.get(i * 3 + 1).copied().unwrap_or_default(),
+                        p.get(i * 3 + 2).copied().unwrap_or_default(),
+                    ),
+                    normal: vec3(
+                        n.get(i * 3).copied().unwrap_or_default(),
+                        n.get(i * 3 + 1).copied().unwrap_or_default(),
+                        n.get(i * 3 + 2).copied().unwrap_or_default(),
+                    ),
+                    tex_coords: vec2(
+                        t.get(i * 2).copied().unwrap_or_default(),
+                        t.get(i * 2 + 1).copied().unwrap_or_default(),
+                    ),
+                });
+            }
 
-        // process material
-        // let mut textures = Vec::new();
-        // if let Some(material_id) = mesh.material_id {
-        //     let material = &materials[material_id];
+            // process material
+            // let mut textures = Vec::new();
+            // if let Some(material_id) = mesh.material_id {
+            //     let material = &materials[material_id];
 
-        //     // 1. diffuse map
-        //     if !material.diffuse_texture.is_empty() {
-        //         let texture =
-        //             self.loadMaterialTexture(&material.diffuse_texture, "texture_diffuse");
-        //         textures.push(texture);
-        //     }
-        //     // 2. specular map
-        //     if !material.specular_texture.is_empty() {
-        //         let texture =
-        //             self.loadMaterialTexture(&material.specular_texture, "texture_specular");
-        //         textures.push(texture);
-        //     }
-        //     // 3. normal map
-        //     if !material.normal_texture.is_empty() {
-        //         let texture = self.loadMaterialTexture(&material.normal_texture, "texture_normal");
-        //         textures.push(texture);
-        //     }
-        //     // NOTE: no height maps
-        // }
+            //     // 1. diffuse map
+            //     if !material.diffuse_texture.is_empty() {
+            //         let texture =
+            //             self.loadMaterialTexture(&material.diffuse_texture, "texture_diffuse");
+            //         textures.push(texture);
+            //     }
+            //     // 2. specular map
+            //     if !material.specular_texture.is_empty() {
+            //         let texture =
+            //             self.loadMaterialTexture(&material.specular_texture, "texture_specular");
+            //         textures.push(texture);
+            //     }
+            //     // 3. normal map
+            //     if !material.normal_texture.is_empty() {
+            //         let texture = self.loadMaterialTexture(&material.normal_texture, "texture_normal");
+            //         textures.push(texture);
+            //     }
+            //     // NOTE: no height maps
+            // }
 
-        meshs.push(Mesh::new(vertices, vec![], indices));
-    }
+            Mesh::new(vertices, vec![], indices)
+        })
+        .collect();
 
     Ok(Model {
         name: String::new(),

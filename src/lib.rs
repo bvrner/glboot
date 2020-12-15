99ef0c08b34aef8e4605743775f312be4c21c8d6
Jonatas <jbmagalhaes28@gmail.com>
2020-12-15 18:44:39 -0300

    Custom slerp function

--- src/lib.rs
+++ src/lib.rs
@@ -59,3 +59,40 @@ impl ImGUI {
         self.imgui_glfw.draw(ui, window);
     }
 }
+
+// cgmath's slerp is bugged
+pub fn slerp(
+    left: cgmath::Quaternion<f32>,
+    right: cgmath::Quaternion<f32>,
+    amount: f32,
+) -> cgmath::Quaternion<f32> {
+    let num2;
+    let num3;
+    let num = amount;
+    let mut num4 = (((left.v.x * right.v.x) + (left.v.y * right.v.y)) + (left.v.z * right.v.z))
+        + (left.s * right.s);
+    let mut flag = false;
+    if num4 < 0.0 {
+        flag = true;
+        num4 = -num4;
+    }
+    if num4 > 0.999_999 {
+        num3 = 1.0 - num;
+        num2 = if flag { -num } else { num };
+    } else {
+        let num5 = num4.acos();
+        let num6 = 1.0 / num5.sin();
+        num3 = ((1.0 - num) * num5).sin() * num6;
+        num2 = if flag {
+            -(num * num5).sin() * num6
+        } else {
+            (num * num5).sin() * num6
+        };
+    }
+    cgmath::Quaternion::new(
+        (num3 * left.s) + (num2 * right.s),
+        (num3 * left.v.x) + (num2 * right.v.x),
+        (num3 * left.v.y) + (num2 * right.v.y),
+        (num3 * left.v.z) + (num2 * right.v.z),
+    )
+}
